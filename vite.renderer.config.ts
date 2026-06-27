94c42b718cd6c060b9cb470a4550d3db631b47cb
XFmorning <xfc2389227173@163.com>
2026-06-27 18:32:06 +0800

    fix: 修复 vite.renderer.config.ts 缺少 root 导致找不到 index.html

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "src/renderer",
   plugins: [react()],
   build: {
     outDir: ".vite/renderer/main_window",
